a1635b8bb8dab8c10bf4cf04871f615d75609a73
Baltukov Nomto <nomto_04@mail.ru>
2022-04-09 12:35:05 +0800

    Спарсил шоколадки из МегаТитан

--- titan.js
+++ titan.js
@@ -32,19 +32,38 @@ const puppeteer = require('puppeteer');
     
 
     
-    await page.screenshot({ path: 'screenshot1.png' }); // Делает скриншот страницы
-    /*
+    await page.screenshot({ path: 'screenshot2.png' }); // Делает скриншот страницы
+   
     let images;
     let finalObj = [];
+    // async function autoScroll(page){
+    //     await page.evaluate(async () => {
+    //         await new Promise((resolve, reject) => {
+    //             var totalHeight = 0;
+    //             var distance = 1000;
+    //             var timer = setInterval(() => {
+    //                 var scrollHeight = document.body.scrollHeight;
+    //                 window.scrollBy(0, distance);
+    //                 totalHeight += distance;
     
-    for (let i = 1; i <= 4; i++) {
-       await page.goto(`https://tgabsolut-shop.ru/catalog/konditerskie-izdeliya2/shokolad/?PAGEN_1=${i}`); // ссылка которую нужно парсить
+    //                 if(totalHeight >= scrollHeight - window.innerHeight){
+    //                     clearInterval(timer);
+    //                     resolve();
+    //                 }
+    //             }, 1000);
+    //         });
+    //     });
+    // }
+    for (let i = 1; i <= 1; i++) {
+       await page.goto(`https://megatitan.ru/catalog/shokoladnye_izdeliya_karamel_iris/shokoladnye_plitki/`); // ссылка которую нужно парсить
         await page.waitForSelector('.catalog_block'); // ждём загрузки того же блока что и сверху
 
-
+        
+        
         images = await page.evaluate(() => {
             let imgElements = document.querySelectorAll('.catalog_block  .image_wrapper_block img'); // получаем каждую картинку
             let priceElements = document.querySelectorAll('.catalog_block .price .price_value'); // получаем каждую цену
+            let titleElement = document.querySelectorAll('.catalog_block .item-title span'); // получаем каждую цену
 
 
             let products = Object.values(imgElements).map((imgElement, index) => {
@@ -53,31 +72,39 @@ const puppeteer = require('puppeteer');
                         price: priceEl.textContent // содержимое дива
                     }
                 })
+                let titles = Object.values(titleElement).map((titleEL)=> { // создаём обьект с ценами
+                    return {
+                        title: titleEL.textContent // содержимое дива
+                    }
+                })
                 return {
                     src: imgElement.src,
                     alt: imgElement.alt,
-                    price: parseInt(Object.values(prices[index])) // Получаем значение у обьекта Цена
+                    price: parseInt(Object.values(prices[index])), // Получаем значение у обьекта Цена
+                    title: Object.values(titles[index]) // Получаем значение у обьекта Имя
                 }
             })
             return products;
         })
         finalObj.push(images); // Тут помучался и сделал объединение всех массивов в один
+        // await autoScroll(page);
+        
 
     }
     let final = finalObj.flat().map((obj, index)=> ({
         id: index,
         src: obj.src,
         alt: obj.alt,
-        price: obj.price
+        price: obj.price,
+        title: obj.title.join('')
     }))
-    fs.writeFile('imagesURL-Absolut.json', JSON.stringify(final, null, ' '), err => { // и ещё выучил метод flat который поднимает навверх
+    fs.writeFile('imagesURL-Titan.json', JSON.stringify(final, null, ' '), err => { // и ещё выучил метод flat который поднимает навверх
         if (err) return err;
         console.log('images > imagesURL.json');
     })
-
-    */
-
+    
 
 
-    // await browser.close();
+
+    await browser.close();
 })();
